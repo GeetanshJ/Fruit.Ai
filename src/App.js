ffd06583cebc61aed636cde8893ec2ea37f06e63
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2024-09-14 18:51:07 +0530

    Add new feature

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import AboutUs from './components/AboutUs';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
     return (
