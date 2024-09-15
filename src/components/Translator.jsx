473713fec63ce17f828b94e6a20a18aa7fc890cc
GeetanshJ <geetansh0513.be21@chitkara.edu.in>
2024-09-15 21:47:14 +0530

    done

--- src/components/Translator.jsx
+++ src/components/Translator.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import "../css/Translator.css"
+import "../css/Translator.css";
 import lang from '../components/languages';
 
 function Translator() {
@@ -9,6 +9,11 @@ function Translator() {
     const [toLanguage, setToLanguage] = useState('hi-IN');
     const [languages, setLanguages] = useState({});
     const [loading, setLoading] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen);
+    };
 
     useEffect(() => {
         setLanguages(lang);
@@ -39,6 +44,7 @@ function Translator() {
         setLoading(true);
         const apiUrl = process.env.REACT_APP_TRANSLATE_API_URL;
         let url = `${apiUrl}?q=${fromText}&langpair=${fromLanguage}|${toLanguage}`;
+        console.log(url);
         fetch(url)
             .then((res) => res.json())
             .then((data) => {
@@ -47,26 +53,10 @@ function Translator() {
             });
     };
 
-    const handleIconClick = (target, id) => {
-        if (!fromText || !toText) return;
 
-        if (target.classList.contains('copy-icon')) {
-            if (id === 'from') {
-                copyContent(fromText);
-            } else {
-                copyContent(toText);
-            }
-        } else {
-            if (id === 'from') {
-                utterText(fromText, fromLanguage);
-            } else {
-                utterText(toText, toLanguage);
-            }
-        }
-    };
 
     return (
-        <div>
+        <div className="translator-container">
             <nav className={`navbar ${isOpen ? 'open' : ''}`}>
                 <div className="navbar-container">
                     <i className='fas fa-apple-alt'></i>
@@ -75,10 +65,9 @@ function Translator() {
                     </div>
                     <ul className={`nav-links ${isOpen ? 'active' : ''}`}>
                         <li><a href="/home">Home</a></li>
-                        <li><a href="">Chat</a></li>
+                        <li><a href="#">Chat</a></li>
                         <li><a href="/">Login</a></li>
                         <li><a href="/faqs">FAQ's</a></li>
-
                     </ul>
                 </div>
             </nav>
@@ -100,11 +89,9 @@ function Translator() {
                         readOnly
                     ></textarea>
                 </div>
-                <ul className="translator-controls">
-                    <li className="control-row source">
+                <div className="translator-controls">
+                    <div className="control-row source">
                         <div className="control-icons">
-                            <i id="from" className="fa-solid fa-volume-high speak-icon" onClick={(e) => handleIconClick(e.target, 'from')}></i>
-                            <i id="from" className="fa-solid fa-copy copy-icon" onClick={(e) => handleIconClick(e.target, 'from')}></i>
                         </div>
                         <select value={fromLanguage} onChange={(e) => setFromLanguage(e.target.value)}>
                             {Object.entries(languages).map(([code, name]) => (
@@ -113,9 +100,11 @@ function Translator() {
                                 </option>
                             ))}
                         </select>
-                    </li>
-                    <li className="exchange-icon" onClick={handleExchange}><i className="fa-solid fa-arrow-right-arrow-left"></i></li>
-                    <li className="control-row target">
+                    </div>
+                    <div className="exchange-icon" onClick={handleExchange}>
+                        <i className="fa-solid fa-arrow-right-arrow-left"></i>
+                    </div>
+                    <div className="control-row target">
                         <select value={toLanguage} onChange={(e) => setToLanguage(e.target.value)}>
                             {Object.entries(languages).map(([code, name]) => (
                                 <option key={code} value={code}>
@@ -124,11 +113,9 @@ function Translator() {
                             ))}
                         </select>
                         <div className="control-icons">
-                            <i id="to" className="fa-solid fa-copy copy-icon" onClick={(e) => handleIconClick(e.target, 'to')}></i>
-                            <i id="to" className="fa-solid fa-volume-high speak-icon" onClick={(e) => handleIconClick(e.target, 'to')}></i>
                         </div>
-                    </li>
-                </ul>
+                    </div>
+                </div>
             </div>
             <button onClick={handleTranslate} disabled={loading}>
                 {loading ? 'Translating...' : 'Translate Text'}
